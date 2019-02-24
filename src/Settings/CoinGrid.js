cd1f359639710d106107139bbb9783010282e092
Preston Davis <35507470+premdav@users.noreply.github.com>
2019-02-24 12:43:26 -0600

    style coin grid

--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
 import { AppContext } from '../App/AppProvider';
+import { SelectableTile } from '../Shared/Tile';
 
 export const CoinGridStyled = styled.div`
     display: grid;
     grid-template-columns: repeat(5, 1fr);
+    grid-gap: 15px;
 `;
 
 const CoinGrid = () => {
@@ -15,7 +17,7 @@ const CoinGrid = () => {
                     <CoinGridStyled>
                         {Object.keys(coinList).map(coinKey => {
                             return (
-                                <div>{coinKey}</div>
+                                <SelectableTile>{coinKey}</SelectableTile>
                             );
                         })}
                     </CoinGridStyled>
