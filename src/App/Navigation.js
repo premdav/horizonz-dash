72461f373a9582c670f978b70a8b5452c5fe7825
Preston Davis <35507470+premdav@users.noreply.github.com>
2019-02-24 10:42:07 -0600

    styling navigation bar

--- src/App/Navigation.js
+++ src/App/Navigation.js
@@ -1,18 +1,44 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+const Logo = styled.div`
+    font-size: 1.5em;
+`;
 
 const Bar = styled.div`
     display: grid;
+    margin-bottom: 40px;
     grid-template-columns: 180px auto 100px 100px;
 `;
+
+const ControlButtonElement = styled.div`
+    cursor: pointer;
+    text-align: center;
+    padding: 2px;
+    ${props => props.active && css`
+        border: 1px solid #697597;
+    `};
+`;
+
+function toProperCase(lower) {
+    return lower.charAt(0).toUpperCase() + lower.substr(1);
+}
+
+const ControlButton = ({name, active}) => {
+    return (
+        <ControlButtonElement active={active}>
+            {toProperCase(name)}
+        </ControlButtonElement>
+    );
+}
 
 const Navigation = () => {
     return (
         <Bar>
-            <div>Horizonz</div>
+            <Logo>Horizonz</Logo>
             <div></div>
-            <div>Dashboard</div>
-            <div>Settings</div>
+            <ControlButton active name="dashboard" />
+            <ControlButton name="settings" />
         </Bar>
     );
 }
