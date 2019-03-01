2132f8be1b465ca6ca67fea451df18d3b21ffc0b
Preston Davis <35507470+premdav@users.noreply.github.com>
2019-03-01 11:32:44 -0600

    added option to change graph between days, weeks, months

--- src/Dashboard/PriceChart.js
+++ src/Dashboard/PriceChart.js
@@ -4,14 +4,24 @@ import { Tile } from '../Shared/Tile';
 import { AppContext } from '../App/AppProvider';
 import ReactHighcharts from 'react-highcharts';
 import HighchartsTheme from './HighchartsTheme';
+import ChartSelect from './ChartSelect';
+
 ReactHighcharts.Highcharts.setOptions(HighchartsTheme);
 
 const PriceChart = () => {
     return (
         <AppContext.Consumer>
-            {({ historical }) => {
+            {({ historical, changeChartSelect }) => {
                 return (
                     <Tile>
+                        <ChartSelect
+                            defaultValue={'months'}
+                            onChange={e => changeChartSelect(e.target.value)}
+                        >
+                            <option value='days'>Days</option>
+                            <option value='weeks'>Weeks</option>
+                            <option value='months'>Months</option> 
+                        </ChartSelect>
                         {
                             historical ? <ReactHighcharts config={highchartsConfig(historical)} />
                             : <div>Loading Historical Data</div>
