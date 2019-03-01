4f545d4c3868912d576cea775b71a673a9b1f25c
Preston Davis <35507470+premdav@users.noreply.github.com>
2019-03-01 11:48:31 -0600

    finishing up

--- src/Dashboard/HighchartsTheme.js
+++ src/Dashboard/HighchartsTheme.js
@@ -1,8 +1,10 @@
+import { lightTheme } from '../Shared/Styles';
+
 const theme = {
     colors: ['#2b908f', '#90ee7e', '#f45b5b', '#7798BF', '#aaeeee', '#ff0066',
         '#eeaaee', '#55BF3B', '#DF5353', '#7798BF', '#aaeeee'],
     chart: {
-        backgroundColor: '#3C4564',
+        backgroundColor: lightTheme ? 'white' : '#3C4564',
         style: {
             fontFamily: '\'Unica One\', sans-serif'
         },
